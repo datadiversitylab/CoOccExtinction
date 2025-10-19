b9823f9130405bed152244a68bd55e15a6bfe3f1
Danielle Van Boxel <the.dvb@gmail.com>
2025-10-18 21:27:05 -0700

    Adjust intersection to occur at file reading level and simplify polygons to about 1km or less resolution

--- srcPy/find_extant_intersect.py
+++ srcPy/find_extant_intersect.py
@@ -5,20 +5,35 @@ import topojson
 import glob
 import tqdm
 import os
+import sys
 
 #eps = .001 # about .1km in lat, variable in lon
 #chunksize = 10
-base = '/home/vanboxel/proj/CoOccExtinction/data'
+#base = '/home/vanboxel/proj/CoOccExtinction/data'
+#mode = 'Assemblage'
+base = sys.argv[1]
+mode = sys.argv[2]
+all_sp = sys.argv[3:]
+eps = .01 # about 1km in lat, variable in lon
 
 cached = True
 
-#for cwd in ['Amphibians', 'Reptiles', 'Mammals']:
-for cwd in ['Mammals']:
-    print(f'Reading {cwd} Extant Shapes')
-    input_gdf = pd.concat([gpd.read_file(fname) for fname in glob.glob(f'{base}/01_raw/{cwd}/extant/*.shp')])
+if mode == 'Community':
+    input_gdf_names = glob.glob(f'{base}/01_raw/*/extant/*.shp')
+    #input_gdf = pd.concat([gpd.read_file(fname) for fname in ])
 
-    out_path = f'{base}/02_intermediate/{cwd}'
+#for cwd in ['Amphibians', 'Reptiles', 'Mammals']:
+#for cwd in ['Birds']:
+for cwd in all_sp:
+    print(f'Reading {cwd} Extant Shapes')
+    if mode == 'Assemblage':
+        input_gdf_names = glob.glob(f'{base}/01_raw/{cwd}/extant/*.shp') + glob.glob(f'{base}/01_raw/{cwd}/extant/*.gpkg')
+        #input_gdf = pd.concat([gpd.read_file(fname) for fname in glob.glob(f'{base}/01_raw/{cwd}/extant/*.shp')] +
+        #                      [gpd.read_file(fname) for fname in glob.glob(f'{base}/01_raw/{cwd}/extant/*.gpkg')])
+
+    out_path = f'{base}/02_intermediate/{cwd}_{mode}'
     extinct = glob.glob(f'{base}/01_raw/{cwd}/extinct/*.gpkg')
+    # need to restart on 7/Numenius_tenuirostris.gpkg
     for sp in tqdm.tqdm(extinct):
         sp_gdf = gpd.read_file(sp)
         sp_name = sp_gdf['SCI_NAME'].iloc[0].replace(' ','_')
@@ -28,17 +43,23 @@ for cwd in ['Mammals']:
             continue
 
         print(sp)
-        mask = None
+        #mask = None
+        big_ints = []
         for g in sp_gdf['geometry']:
-            ints = input_gdf.intersects(g)
-            if mask is None:
-                mask = ints
-            else:
-                mask = mask | ints
+            #ints = input_gdf.intersects(g)
+            all_ints = [gpd.read_file(fname, mask=g) for fname in input_gdf_names]
+            big_ints.extend(all_ints)
+            #if mask is None:
+            #    mask = ints
+            #else:
+            #    mask = mask | ints
         os.makedirs(sp_out, exist_ok=True)
+        sp_gdf['geometry'] = sp_gdf.simplify(eps)
         sp_gdf.to_file(f'{sp_out}/{sp_name}.shp')
 
         os.makedirs(extant_out, exist_ok=True)
-        extant_shp = input_gdf[mask]
+        #extant_shp = input_gdf[mask]
+        extant_shp = pd.concat(big_ints)
+        extant_shp['geometry'] = extant_shp.simplify(eps)
         extant_shp.to_file(f'{extant_out}/extant.shp')
 
--- srcPy/grab_extra_extant_polys.py
+++ srcPy/grab_extra_extant_polys.py
@@ -6,28 +6,45 @@ import glob
 import tqdm
 import os
 import shapely as sh
+import sys
 
 #eps = .001 # about .1km in lat, variable in lon
 #chunksize = 10
-base = '/home/vanboxel/proj/CoOccExtinction/data'
+#base = '/home/vanboxel/proj/CoOccExtinction/data'
+base = sys.argv[1]
+mode = sys.argv[2]
+all_sp = sys.argv[3:]
+eps = .01 # about 1km in lat, variable in lon
 
 cached = True
 
-#for cwd in ['Amphibians', 'Reptiles', 'Mammals']:
-for cwd in ['Reptiles']:
-    print(f'Reading {cwd} Extant Shapes')
-    input_gdf = pd.concat([gpd.read_file(fname) for fname in glob.glob(f'{base}/01_raw/{cwd}/extant/*.shp')])
+if mode == 'Community':
+    input_gdf_names = glob.glob(f'{base}/01_raw/*/extant/*.shp')
+    #input_gdf = pd.concat([gpd.read_file(fname) for fname in glob.glob(f'{base}/01_raw/*/extant/*.shp')])
 
-    out_path = f'{base}/02_intermediate/{cwd}'
-    exts = glob.glob(f'{base}/02_intermediate/{cwd}/*/extant/extant.shp')
+#for cwd in ['Amphibians', 'Reptiles', 'Mammals']:
+for cwd in all_sp:
+    print(f'Reading {cwd} Extant Shapes')
+    if mode == 'Assemblage':
+        #input_gdf = pd.concat([gpd.read_file(fname) for fname in glob.glob(f'{base}/01_raw/{cwd}/extant/*.shp')])
+        input_gdf_names = glob.glob(f'{base}/01_raw/{cwd}/extant/*.shp') + glob.glob(f'{base}/01_raw/{cwd}/extant/*.gpkg')
+
+    out_path = f'{base}/02_intermediate/{cwd}_{mode}'
+    exts = glob.glob(f'{base}/02_intermediate/{cwd}_{mode}/*/extant/extant.shp')
     for ex in tqdm.tqdm(exts):
         sp_gdf = gpd.read_file(ex)
         species = set(sp_gdf['sci_name'])
         all_info = []
         for sp in species:
             # NB: change sp_gdf to input_gdf for actual run
-            rows = input_gdf[input_gdf['sci_name'] == sp]
-            category = rows['category'].iloc[0].lower()
+            #rows = input_gdf[input_gdf['sci_name'] == sp]
+            rows = pd.concat([gpd.read_file(fname, where=f'sci_name = "{sp}"') for fname in input_gdf_names])
+            # different format for birds
+            if cwd == 'Birds':
+                # based on possibly extinct or worse
+                category = 'ex' if min(rows['presence']) > 3 else 'lc'
+            else:
+                category = rows['category'].iloc[0].lower()
             if 'ex' in category or 'ew' in category:
                 continue # skip as extinct
             if len(rows) == 1:
@@ -41,6 +58,6 @@ for cwd in ['Reptiles']:
 
         extant_out = ex[:-10]
         os.makedirs(extant_out, exist_ok=True)
+        new_sp_gdf['geometry'] = new_sp_gdf.simplify(eps)
         new_sp_gdf.to_file(f'{extant_out}/extant_new.shp')
-    del input_gdf
 
