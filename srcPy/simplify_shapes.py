19206ef4c28905ff4bc13b93301a193a292f8511
Danielle Van Boxel <the.dvb@gmail.com>
2025-10-19 17:24:00 -0700

    Add birds to species ints.  Something about logging species names not working

--- srcPy/simplify_shapes.py
+++ srcPy/simplify_shapes.py
@@ -9,31 +9,39 @@ eps = .001 # about .1km in lat, variable in lon
 chunksize = 10
 base = '/home/vanboxel/proj/CoOccExtinction/data/01_raw'
 
-for cwd in ['Amphibians', 'Reptiles', 'Mammals']:
+#for cwd in ['Amphibians', 'Reptiles', 'Mammals']:
+for cwd in ['Birds']:
     output_gdf = []
-    for fname in glob.glob(f'{base}/{cwd}/extant/*.shp'):
+    for fname in glob.glob(f'{base}/{cwd}/extant/*.shp') + glob.glob(f'{base}/{cwd}/extant/*.gpkg'):
         # Read the shapefile
-        print(f'Reading {fname}')
-        input_gdf = gpd.read_file(fname)
+        #print(f'Reading {fname}')
+        start = 0
+        while True:
+            if start % 100 == 0:
+                print(start, end=' ')
+            sql = f'select * from "BOTW_2025" where fid >= {start} and fid < {start+chunksize}' 
+            input_gdf = gpd.read_file(fname, sql=sql)
+            output_gdf.append(input_gdf.simplify(eps))
+            start += chunksize
 
-        #input_gdf = input_gdf.head(chunksize)
-        nchunks = len(input_gdf)//chunksize
-        if len(input_gdf) % chunksize:
-            nchunks += 1
+            #input_gdf = input_gdf.head(chunksize)
+            #nchunks = len(input_gdf)//chunksize
+            #if len(input_gdf) % chunksize:
+            #    nchunks += 1
 
-        # Convert to topology, simplify and convert back to GeoDataFrame
-        ## For safety, loop through chunks of these
-        for rows in tqdm.tqdm(np.array_split(input_gdf, nchunks)): 
-            print(f'Topologizing {fname}')
-            topo = topojson.Topology(rows, prequantize=False)
-            print(f'Simplifying {fname}')
-            topo_simpl = topo.toposimplify(eps)
-            print(f'GDFing {fname}')
-            simpl_gdf = topo_simpl.to_gdf()
-            # Fix any invalid geometries (self-intersections)
-            print(f'Validating {fname}')
-            simpl_gdf.geometry = simpl_gdf.geometry.make_valid()
-            output_gdf.append(simpl_gdf)
+            # Convert to topology, simplify and convert back to GeoDataFrame
+            ## For safety, loop through chunks of these
+            #for rows in tqdm.tqdm(np.array_split(input_gdf, nchunks)): 
+                #print(f'Topologizing {fname}')
+                #topo = topojson.Topology(rows, prequantize=False)
+                #print(f'Simplifying {fname}')
+                #topo_simpl = topo.toposimplify(eps)
+                #print(f'GDFing {fname}')
+                #simpl_gdf = topo_simpl.to_gdf()
+                # Fix any invalid geometries (self-intersections)
+                #print(f'Validating {fname}')
+                #simpl_gdf.geometry = simpl_gdf.geometry.make_valid()
+                #output_gdf.append(input_gdf.simplify(eps))
     output_gdf = pd.concat(output_gdf, ignore_index=True)
 
     # Write to output file, class combined for ease of use
--- srcPy/species2CS.py
+++ srcPy/species2CS.py
@@ -3,30 +3,40 @@ import glob
 import pandas as pd
 import geopandas as gpd
 import tqdm
+import sys
 
 info = []
 
-ddir = '/home/vanboxel/proj/CoOccExtinction/data/'
-base = ddir + '02_intermediate'
-comm = 'Assemblage'
+ddir = sys.argv[1]
+mode = sys.argv[2]
+i = int(sys.argv[3])
+all_sp = sys.argv[4:]
 
-i = 0
-os.makedirs(ddir + 'case_studies', exist_ok=True)
-for cl in ['Amphibians', 'Mammals', 'Reptiles']:
-    for sp_dir in tqdm.tqdm(glob.glob(base + f'/{cl}/*')):
-        sp = os.path.split(sp_dir)[1].lower()
-        info.append((i, sp, 0, comm, cl))
+#ddir = '/home/vanboxel/proj/CoOccExtinction/data/'
+base = ddir + '/02_intermediate'
+#mode = 'Assemblage'
+
+#if mode == 'Community':
+#    input_gdf = pd.concat([gpd.read_file(fname) for fname in glob.glob(f'{base}/01_raw/*/extant/*.shp')])
+
+
+os.makedirs(ddir + '/case_studies', exist_ok=True)
+#for cl in ['Amphibians', 'Mammals', 'Reptiles']:
+for cl in all_sp:
+    for sp_dir in tqdm.tqdm(glob.glob(base + f'/{cl}_{mode}/*')):
+        sp = os.path.split(sp_dir)[1].lower()[:-(len(mode)+1)]
+        info.append((i, sp, 0, mode, cl))
         extants = gpd.read_file(sp_dir + '/extant/extant.shp')
         names = extants['sci_name'].apply(lambda s: s.lower().replace(' ', '_'))
-        info.extend([(i, n, 1, comm, cl) for n in names])
+        info.extend([(i, n, 1, mode, cl) for n in names])
         with open(sp_dir + '/species.csv','w') as f:
             print('\n'.join([sp] + names.tolist()), file=f)
         del extants
-        os.system(f'mv {sp_dir} {ddir}case_studies/CS_{i:03}')
+        os.system(f'mv {sp_dir} {ddir}/case_studies/CS_{i:03}')
         i += 1
 
 summary = pd.DataFrame(info, columns=['cs','species', 'extant', 'community', 'class'])
-summary.to_csv(ddir + 'species.csv')
+summary.to_csv(ddir + f'/species_{mode}.csv', index=False, header=False)
 
         
 
