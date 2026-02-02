6b06a67d965c5b94f13aee2591d2e2eea497f42b
cromanpa94 <cromanpa94@gmail.com>
2026-02-02 15:23:20 -0800

    cell-based using reference raster

--- srcR/pipelines/pipeline_cell.R
+++ srcR/pipelines/pipeline_cell.R
@@ -24,79 +24,43 @@ pipeline_cell <- function(case_studies, rasters, traits, ref){
     
     full_species <- rbind(extant, extinct)
     
-    # CREATE A BUFFER AROUND THE COMBINED RANGE (takes too long)
-    # combined_range <- aggregate(full_species)
-    # buffer_area <- buffer(combined_range, width = 1000) # 1Km
-    # combined_range_proj <- combined_range
-    # study_area <- union(buffer_area, full_species)
     study_area <- full_species
-    
     # CREATE GRID CELLS FOR THE ENTIRE AREA
-    ref_study <- rast(study_area)
     ref_r <- rast(here("data", "spatial_data", "reference_grids", ref))
+    ref_r_study_area <- crop(ref_r, study_area)
+    study_area_r <- rasterize(study_area, ref_r_study_area, field = 1, background = 0)
     
-    #Get the cells in the ref raster that are in the study area
-    grid_cells <- #
-      
     #Extract raster data
-    val_df <- #
-    
-    
-    cell_size <- res(ref)  # Need to fix the resolution
-    # clarified the extents and resolutions
-    grid_extent <- ext(study_area)
-    grid_cells <- rast(grid_extent, resolution=cell_size)
-    grid_cells <- as.polygons(grid_cells)
-    # grid_cells <- crop(grid_cells, study_area) #potentially necessary but very slow for the case study
-    crs(grid_cells)
-    
-    #create latitude and longitude based on centroids
-    centroids<-terra::centroids(grid_cells)
-    coords<-terra::crds(centroids)
-    grid_cells$longitude<-coords[,1]
-    grid_cells$latitude<-coords[,2]
-    # unique cell IDs
-    grid_cells$cell_id <- 1:nrow(grid_cells)
-    
-    # extract all of the raster variables for each cell
-    raster_vals<-lapply(rasters,function(x){
-      terra::extract(x,grid_cells, fun="mean",na.rm=TRUE)[,-1]
-    })
-
-    val_df<-do.call(cbind,raster_vals)
-    colnames(val_df) <- names(rasters)
-    
-    
-    ##CRP: I'm reviewing this component.
-    
-    # CLASSIFY CELLS BASED ON EXTINCT SPECIES PRESENCE
-    
-    # Initialize results dataframe
+    occupied_cells <- which(values(study_area_r, mat = FALSE) > 0)
+    coords <- xyFromCell(study_area_r, occupied_cells)
+    extracted_values <- terra::extract(rasters, coords)
     cell_classification <- data.frame(
-      cell_id = grid_cells$cell_id,
-      longitude=grid_cells$longitude,
-      latitude=grid_cells$latitude,
-      has_extinct_species = FALSE,
-      extinct_species_count = 0,
-      extinct_species_list = NA,
-      in_buffer_zone = FALSE,
-      val_df
+      cell_id = occupied_cells,
+      lon = coords[, 1],
+      lat = coords[, 2],
+      extracted_values
     )
+  
+    # CLASSIFY CELLS BASED ON EXTINCT SPECIES PRESENCE
+    all_points <- vect(cell_classification[, c("lon", "lat")], 
+                       geom = c("lon", "lat"),
+                       crs = crs(extinct))
     
+    # Extract which extinct polygons each point intersects
+    intersections <- relate(all_points, extinct, "intersects")
     
-    # Check each cell for extinct species presence
-    for(i in 1:nrow(grid_cells)) {
-      current_cell <- grid_cells[i,]
-      # Check if cell intersects with extinct species ranges
-      intersects_extinct <- relate(current_cell, extinct, "intersects")
-      if(any(intersects_extinct)) {
-        # Get which extinct species are in this cell
-        species_in_cell <- extinct$SCI_NAME[intersects_extinct]
-        
+    # Initialize columns
+    cell_classification$has_extinct_species <- FALSE
+    cell_classification$extinct_species_count <- 0
+    cell_classification$extinct_species_list <- ""
+    
+    # Process only cells that intersect
+    for(i in 1:nrow(cell_classification)) {
+      if(any(intersections[i, ])) {
+        species_in_cell <- extinct$SCI_NAME[intersections[i, ]]
         cell_classification$has_extinct_species[i] <- TRUE
         cell_classification$extinct_species_count[i] <- length(species_in_cell)
         cell_classification$extinct_species_list[i] <- paste(species_in_cell, collapse = ";")
-        
       }
     }
     write.csv(cell_classification,here("results",paste0(case_study),"cell_based.csv"))
